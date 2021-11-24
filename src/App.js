8a88ff7aa63392afa292759bb2d496ef7170427f
dbtmddus <dbtmddus128@gmail.com>
2021-11-24 18:19:15 +0900

    default react code(with few changes)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let title = ["title1", "title2", "title3" ]
+  let [like, likehandler] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="list">
+          <h3>
+             {title[0]}
+             <span onClick = { ()=> likehandler(like+1) }> ★ </span> {like}
+          </h3>
+          <p> "11월 1일" </p>
+          <hr/>
+        </div>
+        <div className="list">
+          <h3> {title[1]}  </h3>
+          <p> "11월 2일" </p>
+        </div>
+        <div className="list">
+          <h3> {title[2]}  </h3>
+          <p> "11월 3일" </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +32,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
